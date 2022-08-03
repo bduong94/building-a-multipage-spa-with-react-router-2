6090af75372f77ae5be5c11a55ef66a4bec54d87
Brian Duong <brianminhduong@gmail.com>
2022-08-02 21:56:38 -0400

    Connect Pages with URLs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { Route, Switch } from "react-router-dom";
 
+import AllQuotes from "./pages/AllQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
+
 function App() {
   return (
     <Switch>
-      <Route path="/quotes"></Route>
+      <Route path="/quotes">
+        <AllQuotes />
+      </Route>
 
-      <Route path="/quotes/:quoteId"></Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
 
-      <Route path="/new-quote"></Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
     </Switch>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom/client";
-import { Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
+  <BrowserRouter>
     <App />
-  </Router>
+  </BrowserRouter>
 );
