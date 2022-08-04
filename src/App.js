1977191b51ec07ab3768b211a07d3b8a892abd76
Brian Duong <brianminhduong@gmail.com>
2022-08-03 21:26:38 -0400

    Add a 404 error page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AllQuotes from "./pages/AllQuotes";
 import QuoteDetail from "./pages/QuoteDetail";
 import NewQuote from "./pages/NewQuote";
 import Layout from "./components/layout/Layout";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -15,13 +16,14 @@ function App() {
         <Route path="/quotes" exact>
           <AllQuotes />
         </Route>
-
         <Route path="/quotes/:quoteId">
           <QuoteDetail />
         </Route>
-
         <Route path="/new-quote">
           <NewQuote />
+        </Route>
+        <Route path="*">
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
