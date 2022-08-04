d2e8a675e4fcaed3cd5f705f75d9c2538c915e36
Brian Duong <brianminhduong@gmail.com>
2022-08-04 01:15:51 -0400

    Set prompt to appear only if data is not being entered for NewQuote

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -26,6 +26,10 @@ const QuoteForm = (props) => {
     setIsEntering(true);
   };
 
+  const finishEnteringHandler = () => {
+    setIsEntering(false);
+  };
+
   return (
     <>
       <Card>
@@ -55,7 +59,9 @@ const QuoteForm = (props) => {
             <textarea id="text" rows="5" ref={textInputRef}></textarea>
           </div>
           <div className={classes.actions}>
-            <button className="btn">Add Quote</button>
+            <button onClick={finishEnteringHandler} className="btn">
+              Add Quote
+            </button>
           </div>
         </form>
       </Card>
