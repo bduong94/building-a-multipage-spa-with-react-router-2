623c8aaf32dabc2d453145906eaca6dae8a70af8
Brian Duong <brianminhduong@gmail.com>
2022-08-03 21:22:27 -0400

    Implement HighlightedQuote on QuoteDetails component

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,30 @@
 import { useParams, Route } from "react-router-dom";
+
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Brian", text: "Learning React is fun!" },
+  { id: "q2", author: "Nairb", text: "Learning React is great!" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
   const commentPath = `/quotes/${params.quoteId}/comments`;
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote
+        key={quote.id}
+        author={quote.author}
+        text={quote.text}
+      />
       <Route path={commentPath}>
         <Comments />
       </Route>
