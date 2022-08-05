111f73e08919479d85ad39fe50d87adf1940f25a
Brian Duong <brianminhduong@gmail.com>
2022-08-05 19:45:45 -0400

    Get query params value of ssort from URL

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,11 +1,14 @@
 import { Fragment } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 import QuoteItem from "./QuoteItem";
 import classes from "./QuoteList.module.css";
 
 const QuoteList = (props) => {
   const history = useHistory();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const isSortingAscending = queryParams.get("sort") === "asc";
 
   const changeSortingHandler = () => {
     history.push("/quotes?sort=asc");
